5eef362eacb7d5058816ae5d532f34d42c47aa16
AymericLPR <mric31400@gmail.com>
2023-01-09 06:38:23 -0800

    task 13

---
+++ 13-binary_tree_nodes.c
@@ -0,0 +1,21 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_nodes - Entry point
+ * @tree: binary_tree parameter
+ * Return: size_t
+ */
+
+size_t binary_tree_nodes(const binary_tree_t *tree)
+{
+	size_t count = 0;
+
+	if (!tree)
+	  return (0);
+	if (!tree->left && !tree->right)
+	  return(0);
+	count += 1;
+	count += binary_tree_nodes(tree->left);
+	count += binary_tree_nodes(tree->right);
+	return (count);
+}
