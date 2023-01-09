d836a5d39a91ebd2ec57e15ac6255d55ed654c52
adamdiallo2k <adamdiallo2k@gmail.com>
2023-01-09 06:10:43 -0800

    task 1

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -14,6 +14,8 @@ binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 {
 	binary_tree_t *newnode = malloc(sizeof(binary_tree_t));
 
+	if (!parent)
+		return (0);
 
 	if (!newnode)
 		return (0);
