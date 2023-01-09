d100a1dfd58dc6e8d5b1c57233df045525ff7e34
Aymeric <71835277+AymericLPR@users.noreply.github.com>
2023-01-09 15:41:24 +0100

    Update 13-binary_tree_nodes.c

--- 13-binary_tree_nodes.c
+++ 13-binary_tree_nodes.c
@@ -11,9 +11,9 @@ size_t binary_tree_nodes(const binary_tree_t *tree)
 	size_t count = 0;
 
 	if (!tree)
-	  return (0);
+		return (0);
 	if (!tree->left && !tree->right)
-	  return(0);
+		return (0);
 	count += 1;
 	count += binary_tree_nodes(tree->left);
 	count += binary_tree_nodes(tree->right);
