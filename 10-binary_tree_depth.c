8949b211f6ce201ab84cb005acdce6c16dc10c99
AymericLPR <mric31400@gmail.com>
2023-01-09 04:55:34 -0800

    task 10

---
+++ 10-binary_tree_depth.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "binary_trees.h"
+
+/**
+* binary_tree_node - Entry point
+* Return: size_t
+* @tree: binary_tree parameter
+*/
+
+size_t binary_tree_depth(const binary_tree_t *tree)
+{
+  size_t count = 0;
+  
+	if (!tree || !tree->parent)
+		return (count);
+	count = 1 + binary_tree_depth(tree->parent);
+	return (count);
+}
