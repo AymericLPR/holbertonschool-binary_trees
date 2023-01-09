b3b970b3af2313afe54cecd0f5d175edacd1e690
Aymeric <71835277+AymericLPR@users.noreply.github.com>
2023-01-09 15:06:10 +0100

    Update 12-binary_tree_leaves.c

--- 12-binary_tree_leaves.c
+++ 12-binary_tree_leaves.c
@@ -7,13 +7,13 @@
  */
 size_t binary_tree_leaves(const binary_tree_t *tree)
 {
-  size_t count = 0;
+size_t count = 0;
 
-  if (!tree)
-    return (0);
-  if (!tree->left && !tree->right)
-    return (1);
-  count += binary_tree_leaves(tree->left);
-  count += binary_tree_leaves(tree->right);
-  return (count);
+if (!tree)
+return (0);
+if (!tree->left && !tree->right)
+return (1);
+count += binary_tree_leaves(tree->left);
+count += binary_tree_leaves(tree->right);
+return (count);
 }
