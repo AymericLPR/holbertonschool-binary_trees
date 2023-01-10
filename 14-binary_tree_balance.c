5c3d47b65589b15c7a4b4cd410fd34a87ca62fab
Aymeric <71835277+AymericLPR@users.noreply.github.com>
2023-01-10 10:39:31 +0100

    Update 14-binary_tree_balance.c

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -8,17 +8,17 @@
 
 size_t binary_tree_height(const binary_tree_t *tree)
 {
-  size_t left = 0, right = 0;
+	size_t left = 0, right = 0;
 
-  if (!tree || (!tree->left && !tree->right))
-    return (0);
-  left = 1 + binary_tree_height(tree->left);
-  right = 1 + binary_tree_height(tree->right);
-  if (left > right)
-    return (left);
-  return (right);
+	if (!tree || (!tree->left && !tree->right))
+		return (0);
+	left = 1 + binary_tree_height(tree->left);
+	right = 1 + binary_tree_height(tree->right);
+	if (left > right)
+		return (left);
+	return (right);
 }
-     
+
 /**
  * binary_tree_balance - Entry point
  * @tree: binary_tree parameter
@@ -27,8 +27,8 @@ size_t binary_tree_height(const binary_tree_t *tree)
 
 int binary_tree_balance(const binary_tree_t *tree)
 {
-  size_t left_height = 0;
-    size_t right_height = 0;
+	size_t left_height = 0;
+	size_t right_height = 0;
 
 	if (!tree)
 		return (0);
